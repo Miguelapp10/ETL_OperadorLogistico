24385a315cf693b92c8433476be224247bfc5311
Miguel Angel Pazos Peña <114699192+Miguelapp10@users.noreply.github.com>
2025-07-18 14:47:54 -0500

    Update Simpliroute.py

--- Simpliroute.py
+++ Simpliroute.py
@@ -1,25 +1,41 @@
+import time
+import datetime as dt  # Para fechas (opcional)
+import glob  # Para jalar todo los archivos en una carpeta
+import os  # Para trabajar con rutas
+import locale
+from io import StringIO  # Usado para definir función nueva
+from pathlib import Path
+import numpy as np  # Para operaciones matemáticas
+import xlsxwriter  # Funcionalidad para trabajar con archivos Excel
+from pandas import ExcelWriter  # Para exportar tabla a Excel
+from xlsx2csv import Xlsx2csv  # Usado para definir función nueva
+from decimal import Decimal
+import re
+import unidecode
 import requests
 import json
 import pandas as pd
 from datetime import datetime, timedelta
-from concurrent.futures import ThreadPoolExecutor
 import geopandas as gpd
 from shapely.geometry import Point, LineString
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-#from google.colab import files
-#from google.colab import auth
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3.util.retry import Retry
 from io import BytesIO
 from google.cloud import bigquery
+import pytz
 import warnings
 from pandas_gbq import to_gbq 
-from concurrent.futures import ThreadPoolExecutor, as_completed  # Asegúrate de importar as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed 
+# Definir las zonas horarias
+utc = pytz.UTC
+lima_tz = pytz.timezone("America/Lima")
 # Suprimir sólo las advertencias de InsecureRequestWarning
 warnings.filterwarnings('ignore', category=InsecureRequestWarning)
 
 # Información de autenticación
 token = ''
+
 base_url_vehicles = 'https://api.simpliroute.com/v1/routes/vehicles/'
 base_url_routes = 'https://api.simpliroute.com/v1/routes/routes/'
 base_url_visits = 'https://api.simpliroute.com/v1/routes/visits/'
@@ -36,7 +52,7 @@ def date_range(start_date, end_date):
         yield start_date + timedelta(n)
 
 # Definir el rango de fechas
-start_date = datetime.strptime('2024-04-01', '%Y-%m-%d')
+start_date = datetime.strptime('2025-01-01', '%Y-%m-%d') ##'2024-04-01'
 end_date = datetime.today()  # Usar la fecha actual para end_date
 
 # Función para obtener datos de una URL en una fecha específica
@@ -81,34 +97,56 @@ df_visits = pd.json_normalize(all_visits_data)
 
 # Seleccionar las columnas deseadas de cada DataFrame
 df_vehicles_selected = df_vehicles[['id','name',
-                                    #'capacity','capacity_2','default_driver','location_start_address',
-                                    #'location_start_latitude','location_start_longitude','location_end_address',
-                                    #'location_end_latitude','location_end_longitude','created','modified',
-                                    'color']].drop_duplicates()
-# Renombrar la columna 'id' a 'vehicle'
-df_vehicles_selected = df_vehicles_selected.rename(columns={'id': 'vehicle'})
+                                   #'shift_start','shift_end','capacity','capacity_2','capacity_3','default_driver','location_start_address',
+                                   #'location_start_latitude','location_start_longitude','location_end_address','location_end_latitude',
+                                   #'location_end_longitude','skills','created','modified',
+                                   'color',
+                                   #'reference_id','license_plate','cost','min_load','min_load_2','min_load_3','max_visit',
+                                   #'rest_time_start','rest_time_end','rest_time_duration','deleted','max_time','codrivers','status',
+                                   ]].drop_duplicates().rename(columns={'id': 'vehicle'})
 
 df_routes_selected = df_routes[['id','vehicle','driver','plan','status','planned_date','estimated_time_start',
-                                'estimated_time_end','total_duration','total_distance','total_load','total_load_percentage',
-                                'location_start_address','location_start_latitude','location_start_longitude',
-                                'location_end_address','location_end_latitude','location_end_longitude','start_time',
-                                'end_time','created','modified','kilometers','total_visits','latitude_init','longitude_init',
-                                'latitude_finish','longitude_finish'
-]].drop_duplicates()  # Reemplaza 'other_columns' con las columnas deseadas de df_routes
-# Renombrar la columna 'id' a 'vehicle'
-df_routes_selected = df_routes_selected.rename(columns={'id': 'route','status':'status_route','created':'created_route','modified':'modified_route'})
+                                'estimated_time_end','total_duration','total_distance','total_load',
+                                'total_load_percentage','total_load_2','total_load_2_percentage','total_load_3',
+                                'total_load_3_percentage','location_start_address','location_start_latitude',
+                                'location_start_longitude','location_end_address','location_end_latitude',
+                                'location_end_longitude','comment','start_time','end_time','created','modified',
+                                'kilometers','total_visits','latitude_init','longitude_init','latitude_finish',
+                                'longitude_finish','is_revised',
+                                'reference']].drop_duplicates().rename(columns={'id': 'route',
+                                'status':'status_route','created':'created_route','modified':'modified_route',
+                                'reference':'reference_x'})
+
+df_routes_selected.loc[:, 'comment'] = df_routes_selected['comment'].fillna('')
+df_routes_selected.loc[:, 'reference_x'] = df_routes_selected['reference_x'].fillna('')
 
 # Seleccionar las columnas deseadas
-selected_columns = ['id', 'order', 'tracking_id', 'status', 'title', 'address',  'latitude', 'longitude',
+selected_columns_v1 = ['id', 'order', 'tracking_id', 'status', 'title', 'address',  'latitude', 'longitude',
                     #'load', 'load_2', 'load_3', 'window_start', 'window_end', 'window_start_2', 'window_end_2', 'duration',
-                        'contact_name', 'contact_phone', 'reference', 'notes', 'planned_date', 'route',
-                        'route_estimated_time_start', 'estimated_time_arrival', 'estimated_time_departure', 'checkin_time',
-                        'checkout_time', 'checkout_latitude', 'checkout_longitude', 'checkout_comment',
-                        'checkout_observation',# 'signature','pictures',
-                        'created', 'modified', 'eta_predicted',
-                        'eta_current', 'driver', 'vehicle', 'on_its_way']
-df_visits_selected = df_visits[selected_columns]
+                    'contact_name', 'contact_phone', 'contact_email','reference', 'notes', #'skills_required','skills_optional','tags',
+                    'planned_date', 'programmed_date', 'route',
+                    'route_estimated_time_start', 'estimated_time_arrival', 'estimated_time_departure', 'checkin_time',
+                    'checkout_time', 'checkout_latitude', 'checkout_longitude', 'checkout_comment',
+                    'checkout_observation',# 'signature','pictures',
+                    'created', 'modified', 'eta_predicted','eta_current', 'driver', 'vehicle', 'priority','has_alert',
+                    'priority_level','geocode_alert','visit_type','current_eta','fleet','on_its_way','seller',
+                    'extra_field_values.visitado',
+                    'extra_field_values',
+                        ]
+#df_visits_selected = df_visits[selected_columns_v1] 
 
+df_visits_selected = df_visits[selected_columns_v1].copy()
+
+df_visits_selected.loc[:, 'programmed_date'] = df_visits_selected['programmed_date'].fillna('')
+df_visits_selected.loc[:, 'geocode_alert'] = df_visits_selected['geocode_alert'].fillna('')
+df_visits_selected.loc[:, 'visit_type'] = df_visits_selected['visit_type'].fillna('')
+df_visits_selected.loc[:, 'fleet'] = df_visits_selected['fleet'].fillna('')
+df_visits_selected.loc[:, 'seller'] = df_visits_selected['seller'].fillna('')
+
+# Renombrar las columnas con nombres no válidos
+df_visits_selected.columns = [
+    col.replace('.', '_') for col in df_visits_selected.columns
+]
 # Renombrar la columna 'id' a 'vehicle'
 df_visits_selected = df_visits_selected.rename(columns={'order': 'order_visit'})
 
@@ -135,7 +173,10 @@ def map_observation_to_new_column(observation):
         'bc17ba7d-f735-4aec-ab8f-2baac8772c00':'Siniestro OL',
         '0a782b98-7998-48bf-89d7-7be410a98a55':'Dirección de Agencia',
         'c8be72af-c364-4169-9742-1346519b6a86':'Zona peligrosa',
-        'c5ed62de-19fa-4c47-8e7c-193f3fd8573e':'Sin Información'
+        'c5ed62de-19fa-4c47-8e7c-193f3fd8573e':'Sin Información',
+        '086f1611-2ee6-48c7-984a-5106acc8b631':'Fuera De Horario',
+        '362584e0-8fdf-4a59-82b5-f6283745c4f8':'Rechazado Por Cliente',
+        '34641928-f1b8-4f19-af63-2077649a9d5e':'Cliente Ausente'
         }
     return mapping.get(observation, 'Otro')
 
@@ -148,25 +189,64 @@ df_routes_vehicles = pd.merge(df_routes_selected, df_vehicles_selected, on='vehi
 # Unir los DataFrames en el campo común 'vehicle'
 df_visits_routes_vehicles = pd.merge(df_visits_selected, df_routes_vehicles, on=('vehicle','driver' ,'route', 'planned_date'), how='left')
 
-project_id = ''
+# Convertir 'contact_name' a mayúsculas
+df_visits_routes_vehicles['tienda_name'] = df_visits_routes_vehicles['contact_name'].str.upper()
 
+# Función para extraer la parte derecha de 'title' después del primer '-' o dejarlo igual si no hay '-'
+def extract_tienda_name(row):
+    if pd.isna(row['tienda_name']) or row['tienda_name'].strip() == '':
+        if '-' in row['title']:  # Verifica si hay un guion antes de hacer split
+            return row['title'].split('-', 1)[1].strip()
+        else:
+            return row['title'].strip()  # Si no hay guion, usa el título completo
+    return row['tienda_name']
+
+# Aplicar la función
+df_visits_routes_vehicles['tienda_name'] = df_visits_routes_vehicles.apply(extract_tienda_name, axis=1)
+
+# Función que intenta convertir las fechas considerando microsegundos o no
+def convert_to_datetime(date_str):
+    try:
+        return pd.to_datetime(date_str, format="%Y-%m-%dT%H:%M:%S.%fZ", errors='raise')
+    except ValueError:
+        return pd.to_datetime(date_str, format="%Y-%m-%dT%H:%M:%SZ", errors='raise')
+
+# Convertir las columnas de fecha
+df_visits_routes_vehicles["current_eta"] = df_visits_routes_vehicles["current_eta"].apply(convert_to_datetime)
+df_visits_routes_vehicles["checkin_time"] = df_visits_routes_vehicles["checkin_time"].apply(convert_to_datetime)
+df_visits_routes_vehicles["checkout_time"] = df_visits_routes_vehicles["checkout_time"].apply(convert_to_datetime)
+df_visits_routes_vehicles["created"] = df_visits_routes_vehicles["created"].apply(convert_to_datetime)
+df_visits_routes_vehicles["modified"] = df_visits_routes_vehicles["modified"].apply(convert_to_datetime)
+df_visits_routes_vehicles["start_time"] = df_visits_routes_vehicles["start_time"].apply(convert_to_datetime)
+df_visits_routes_vehicles["created_route"] = df_visits_routes_vehicles["created_route"].apply(convert_to_datetime)
+df_visits_routes_vehicles["modified_route"] = df_visits_routes_vehicles["modified_route"].apply(convert_to_datetime)
+
+# Restar 5 horas
+df_visits_routes_vehicles["current_eta"] = df_visits_routes_vehicles["current_eta"] - timedelta(hours=5)
+df_visits_routes_vehicles["checkin_time"] = df_visits_routes_vehicles["checkin_time"] - timedelta(hours=5)
+df_visits_routes_vehicles["checkout_time"] = df_visits_routes_vehicles["checkout_time"] - timedelta(hours=5)
+df_visits_routes_vehicles["created"] = df_visits_routes_vehicles["created"] - timedelta(hours=5)
+df_visits_routes_vehicles["modified"] = df_visits_routes_vehicles["modified"] - timedelta(hours=5)
+df_visits_routes_vehicles["start_time"] = df_visits_routes_vehicles["start_time"] - timedelta(hours=5)
+df_visits_routes_vehicles["created_route"] = df_visits_routes_vehicles["created_route"] - timedelta(hours=5)
+df_visits_routes_vehicles["modified_route"] = df_visits_routes_vehicles["modified_route"] - timedelta(hours=5)
+
+project_id = ''
 # Configuración del dataset y la tabla en BigQuery
 dataset_id = ''
-table_id = 'SimpliRoute_visits_IL'
-
+table_id = ''
 
 # Carga el DataFrame a BigQuery
 to_gbq(df_visits_routes_vehicles,destination_table=f"{dataset_id}.{table_id}",
                    project_id=project_id,
                    if_exists="replace")  # Opciones: "append", "replace", "fail"
-
 print("Los datos se han cargado exitosamente en BigQuery.")
 
 #################################################################################################################################################
 #################################################################################################################################################
 
 # Información de autenticación
-token_qolqas = ''
+token_qolqas = '42898f92de610efb5b6455f6562d9b61a324e08d'
 base_url_vehicles_qolqas = 'https://api.simpliroute.com/v1/routes/vehicles/'
 base_url_routes_qolqas = 'https://api.simpliroute.com/v1/routes/routes/'
 base_url_visits_qolqas = 'https://api.simpliroute.com/v1/routes/visits/'
@@ -183,7 +263,7 @@ def date_range(start_date_qolqas, end_date_qolqas):
         yield start_date_qolqas + timedelta(n)
 
 # Definir el rango de fechas
-start_date_qolqas = datetime.strptime('2024-04-01', '%Y-%m-%d')
+start_date_qolqas = datetime.strptime('2025-01-01', '%Y-%m-%d') ## '2024-04-01'
 end_date_qolqas  = datetime.today()  # Usar la fecha actual para end_date
 
 # Función para obtener datos de una URL en una fecha específica
@@ -259,7 +339,6 @@ selected_columns = ['id', 'order', 'tracking_id', 'status', 'title', 'address', 
                         'priority_level',
                         'geocode_alert',#'visit_type','current_eta','fleet',
                         'on_its_way',#'seller','extra_field_values',
-
                         ]
 df_visits_qolqas_selected = df_visits_qolqas[selected_columns]
 
@@ -276,7 +355,7 @@ df_routes_vehicles_qolqas = pd.merge(df_routes_qolqas_selected, df_vehicles_qolq
 df_visits_routes_vehicles_qolqas = pd.merge(df_visits_qolqas_selected,df_routes_vehicles_qolqas , on=('vehicle','driver' ,'route', 'planned_date'), how='left')
 
 # Configuración del dataset y la tabla en BigQuery
-table_id_1= 'SimpliRoute_visits_Qolqas'
+table_id_1= ''
 
 # Carga el DataFrame a BigQuery
 to_gbq(df_visits_routes_vehicles_qolqas,
