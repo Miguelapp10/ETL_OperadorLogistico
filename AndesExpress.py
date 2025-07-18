22cee3c72a3e4759ca6b5c6791aaa7de8ebcbcdd
Miguel Angel Pazos Peña <114699192+Miguelapp10@users.noreply.github.com>
2025-07-18 14:40:11 -0500

    Update AndesExpress.py

--- AndesExpress.py
+++ AndesExpress.py
@@ -1,6 +1,35 @@
+import time
+import datetime as dt  # Para fechas (opcional)
+import glob  # Para jalar todo los archivos en una carpeta
+import os  # Para trabajar con rutas
+import locale
+from io import StringIO  # Usado para definir función nueva
+from pathlib import Path
+import numpy as np  # Para operaciones matemáticas
+import xlsxwriter  # Funcionalidad para trabajar con archivos Excel
+from pandas import ExcelWriter  # Para exportar tabla a Excel
+from xlsx2csv import Xlsx2csv  # Usado para definir función nueva
+from decimal import Decimal
+import re
+import unidecode
+import requests
+import json
+import pandas as pd
+from datetime import datetime, timedelta
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+from requests.adapters import HTTPAdapter
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3.util.retry import Retry
+from io import BytesIO
+from google.cloud import bigquery
+import pytz
+import warnings
+from pandas_gbq import to_gbq 
+from concurrent.futures import ThreadPoolExecutor, as_completed 
+
 # Suprimir advertencias SSL
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 # URL de inicio de sesión y consulta
 login_url = "https://andesexpress.pe/vAuthenticate_2.php"
 consulta_url_datos = "https://andesexpress.pe/api_consulta_guia_export.php"
@@ -28,13 +57,12 @@ if login_response.status_code == 200 and "location.replace" in login_response.te
 # Configurar fechas inicial y final
 fecha_fin = datetime.now()
 fecha_inicio = fecha_fin - timedelta(days=30)
-
+fecha_limite = datetime(2024,11, 1) # Fecha de inicio de los datos
 # Bucle para descargar por tramos de 90 días
 consolidated_df = pd.DataFrame()
-
-while fecha_inicio >= datetime(2023, 7, 1):  # Ajusta esta fecha según el inicio de los datos
+ 
+while fecha_inicio >= fecha_limite:  # Comparación correcta (2023, 7, 1)
     print(f"Procesando rango: {fecha_inicio.strftime('%Y-%m-%d')} a {fecha_fin.strftime('%Y-%m-%d')}")
-
     consulta_payload = {
         "c_cli": "2663078",
         "f_ini": fecha_inicio.strftime("%Y-%m-%d"),
@@ -47,7 +75,6 @@ while fecha_inicio >= datetime(2023, 7, 1):  # Ajusta esta fecha según el inici
 
     # Paso 2: Realizar consulta a la API
     consulta_response = session.get(consulta_url_datos, params=consulta_payload, headers=headers, verify=False)
-    
     if consulta_response.status_code == 200:
         print("Consulta exitosa.")
         # Si el contenido de la respuesta es un archivo Excel:
@@ -99,20 +126,19 @@ while fecha_inicio >= datetime(2023, 7, 1):  # Ajusta esta fecha según el inici
                
                 # Selección de columnas y conversión a string
                 df[['Nro_de_guia','Estado','Servicio','Peso_kg','Origen','Destino','Remitente','Direccion_Origen','Consignatario',
-                    'Direccion_Destino', 'Observacion_Estado_Actual''Doc_Cliente','Trasnporte','Obs_Adicional','Nro_Pedido', 'Nro_Consultora',
-                    'Ultima_Ocurrencia','Descripcion', 'Observacion_Visita1','Observacion_Visita2','Observacion_Visita3','Observacion_Visita4'
+                    'Direccion_Destino', 'Observacion_Estado_Actual','Doc_Cliente','Trasnporte','Obs_Adicional','Nro_Pedido', 'Nro_Consultora',
+                    'Ultima_Ocurrencia','Descripcion', 'Observacion_Visita1','Observacion_Visita2','Observacion_Visita3','Observacion_Visita4',
                     'Nro_Documento_Destinatario', 'Departamento_Destinatario','Provincia_Destinatario','Distrito_Destinatario', 
                     'Coordenadas_Entrega','Telefono','Bultos','Ubigeo_Origen','Ubigeo_Destino' ]] = \
                  df[['Nro_de_guia','Estado','Servicio','Peso_kg','Origen','Destino','Remitente','Direccion_Origen','Consignatario',
-                    'Direccion_Destino', 'Observacion_Estado_Actual''Doc_Cliente','Trasnporte','Obs_Adicional','Nro_Pedido', 'Nro_Consultora',
-                    'Ultima_Ocurrencia','Descripcion', 'Observacion_Visita1','Observacion_Visita2','Observacion_Visita3','Observacion_Visita4'
+                    'Direccion_Destino', 'Observacion_Estado_Actual','Doc_Cliente','Trasnporte','Obs_Adicional','Nro_Pedido', 'Nro_Consultora',
+                    'Ultima_Ocurrencia','Descripcion', 'Observacion_Visita1','Observacion_Visita2','Observacion_Visita3','Observacion_Visita4',
                     'Nro_Documento_Destinatario', 'Departamento_Destinatario','Provincia_Destinatario','Distrito_Destinatario', 
                     'Coordenadas_Entrega','Telefono','Bultos','Ubigeo_Origen','Ubigeo_Destino' ]].astype(str)
 
                 # Concatenar datos al DataFrame consolidado
                 if not df.empty:
                     consolidated_df = pd.concat([consolidated_df, df.dropna(how='all', axis=1)], ignore_index=True)
-                #consolidated_df = pd.concat([consolidated_df, df], ignore_index=True)
 
                 print(f"Datos procesados para rango {fecha_inicio.strftime('%Y-%m-%d')} a {fecha_fin.strftime('%Y-%m-%d')}")
             except Exception as e:
